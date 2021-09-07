2dcb495d2a6b055121cec52e913f034711d9b52c
T. Kurt Bond <tkurtbond@gmail.com>
2021-09-07 13:29:14 -0400

    Add a file for helping with my blog.

--- init.el
+++ init.el
@@ -186,6 +186,7 @@
 (load-file "~/lib/emacs/tkb/tkb-time-expansion.el")
 (load-file "~/lib/emacs/tkb/tkb-time-expansion-keys.el")
 (load-file "~/lib/emacs/tkb/tkb-status-reports.el")
+(load-file "~/lib/emacs/tkb/tkb-blog.el")
 
 
 ;;(setq epg-gpg-program "gpg2") ; on macOS with homebrew it's gpg.
---
+++ tkb-blog.el
@@ -0,0 +1,21 @@
+;;; tkb-blog.el --- things to help with my blog      -*- lexical-binding: t; -*-
+;; Author: T. Kurt Bond <tkurtbond@gmail.com>
+
+(defvar tkb-blog-edited-string
+  "\n\n*Last edited: 2021-07-15 17:46:40 EDT*
+
+..
+   Local Variables:
+   time-stamp-format: \"%Y-%02m-%02d %02H:%02M:%02S %Z\"
+   time-stamp-start: \"\\\\*Last edited:[ \\t]+\\\\\\\\?\"
+   time-stamp-end: \"\\\\*\\\\\\\\?\\n\"
+   time-stamp-line-limit: -20
+   End:\n")
+
+(defun tkb-add-blog-edited ()
+  (interactive)
+  (unless (save-excursion
+            (goto-char (point-min))
+            (search-forward "*Last edited:" nil t))
+    (goto-char (point-max))
+    (insert tkb-blog-edited-string)))
