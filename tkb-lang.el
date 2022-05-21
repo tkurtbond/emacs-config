0d519f78bb43eb04e30a3243364406f918a91a44
T. Kurt Bond <tkurtbond@gmail.com>
2022-05-21 07:42:48 -0400

    Add extensions to auto-mode-alist for oberon-mode.

--- tkb-lang.el
+++ tkb-lang.el
@@ -305,4 +305,8 @@
    (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))))
 
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+(add-to-list 'auto-mode-alist '("\\.def$" . oberon-mode))
+(add-to-list 'auto-mode-alist '("\\.obn$" . oberon-mode))
+;;(add-to-list 'auto-mode-alist '("\\.Mod$" . oberon-mode))
 ;;; end of tkb-lang.el
