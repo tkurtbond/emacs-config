e8d36a277f5112023109a8c6dc395b64ff766ce6
T. Kurt Bond <tkurtbond@gmail.com>
2022-12-09 16:10:23 -0500

    Use old-ada-mode instead of ada-mode, since the latter links to repo.

--- unix-init.el
+++ unix-init.el
@@ -80,7 +80,7 @@
   (add-to-list 'load-path tkb-emacs-dir))
 
 (cl-loop for d across ["~/lib/emacs/others" "~/lib/emacs/others/misc"
-                       "~/lib/emacs/others/ada-mode"]
+                       "~/lib/emacs/others/old-ada-mode"]
       do (progn
 	   (when-directory (o (expand-file-name d))
 	     (message "adding %s to load-path" o)
