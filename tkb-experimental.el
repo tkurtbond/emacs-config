4c0033566ecad91ff0e24b6a56d0f0d9c65b198d
T. Kurt Bond <tkurtbond@gmail.com>
2023-01-10 17:04:12 -0500

    Refine tkb-microblog for new yearly blog directories.

--- tkb-experimental.el
+++ tkb-experimental.el
@@ -2705,17 +2705,32 @@ and make it the current selection."
 (defvar tkb-microblog-repo "~/Repos/microblog"
   "Location of the git repository for my microblog.")
 
-(defun tkb-microblog (specify-date-p microblog-title)
-  "Create a Gemtext document for the current day in my microblog."
-  (interactive "P\nsMicroblog title? ")
+(defun tkb-get-iso8601-date ()
+  (interactive)
+  (let* ((default-date-string (format-time-string "%F"))
+         (date (iso8601-parse-date
+                (read-string "Date? " default-date-string))))
+    (setf (car date) 0)
+    (setf (cadr date) 0)
+    (setf (caddr date) 0)
+    date))
+
+(defun tkb-microblog (specify-date-p)
+  "Create a Gemtext document for the current day in my microblog.  A prefix
+argument of - just opens the blog entry for the currrent day without adding
+to it.  Any other prefix argument prompts for the date to use for the blog
+entry instead."
+  (interactive "P")
+  (message "specify-date-p: %S" specify-date-p)
   (let* ((title-sep " - ")
-         (date (format-time-string "%F"))
-         (date (if specify-date-p
-                   (format-time-string
-                    "%F" (iso8601-parse-date (read-string "Date? " date)))
-                 date))
-         (time (format-time-string "%H:%M %Z"))
-         (filename date)
+         (date (if (and specify-date-p (not (eq specify-date-p '-)))
+                   (encode-time (tkb-get-iso8601-date))
+                 (current-time)))
+         (date-string (format-time-string "%F" date))
+         (year-string (format-time-string "%Y" date))
+         (time-string (format-time-string "%H:%M %Z"))
+         (dirname year-string)
+         (filename date-string)
          ;; I thought about doing this:
          ;;(filename (concat date "-"
          ;;                  (tkb-sanitize-for-filename microblog-title)))
@@ -2723,31 +2738,39 @@ and make it the current selection."
          ;; shouldn't be in the name.
          (gemtext-filename    (concat filename ".gmi"))
          (html-filename       (concat filename ".html"))
+         (relative-html-filename (f-join dirname html-filename))
          (microblog-directory (f-join tkb-microblog-repo "gmi" "blog"))
-         (gemtext-pathname    (f-join microblog-directory gemtext-filename))
+         (microblog-entry-directory (f-join microblog-directory dirname))
+         (gemtext-pathname    (f-join microblog-entry-directory
+                                      gemtext-filename))
          (blog-index-pathname (f-join microblog-directory "blog.gmi"))
          (already-exists-p    (f-exists-p gemtext-pathname)))
+    (unless (f-exists-p microblog-entry-directory)
+      (make-directory microblog-entry-directory t))
     (find-file gemtext-pathname)
-    (cond
-     (already-exists-p
-      (goto-char (point-max))
-      (beginning-of-line)
-      (unless (looking-at "^[ \t]*$")
-        (end-of-line)
-        (insert "\n"))
-      (insert "\n## " time title-sep microblog-title "\n"))
-     (t
-      (let ((buf (find-file-noselect blog-index-pathname)))
-        (save-excursion
-          (with-current-buffer buf
-            ;; Save it in the blog index, blog.gmi
-            (if (re-search-forward "^=>" nil t)
-                (beginning-of-line))
-            (insert "=> " html-filename " " date " " time
-                    title-sep microblog-title
-                    "\n")
-            (save-buffer)))
-        (insert "# " date " " time title-sep microblog-title))))))
+
+    (unless (and specify-date-p (eq specify-date-p '-))
+      (let ((microblog-title (read-string "Microblog title? ")))
+      (cond
+       (already-exists-p
+        (goto-char (point-max))
+        (beginning-of-line)
+        (unless (looking-at "^[ \t]*$")
+          (end-of-line)
+          (insert "\n"))
+        (insert "\n## " time-string title-sep microblog-title "\n\n"))
+       (t
+        (let ((buf (find-file-noselect blog-index-pathname)))
+          (save-excursion
+            (with-current-buffer buf
+              ;; Save it in the blog index, blog.gmi
+              (if (re-search-forward "^=>" nil t)
+                  (beginning-of-line))
+              (insert "=> " relative-html-filename " " date-string " "
+                      time-string title-sep microblog-title "\n")
+              (save-buffer)))
+          (insert "# " date-string " " time-string title-sep
+                  microblog-title))))))))
       
 
 (message "End of tkb-experimental.el")
