478ca329a5f5bffb11952a12e4979e99785d524a
T. Kurt Bond <tkurtbond@gmail.com>
2023-02-06 18:24:48 -0500

    Make all the cl-* symbols indent right in emacs lisp.

--- tkb-lang.el
+++ tkb-lang.el
@@ -234,6 +234,14 @@
 (put 'register-groups-bind 'lisp-indent-function 2)
 (put 'ppcre:register-groups-bind 'lisp-indent-function 2)
 
+(load-library "cl-indent") ; defines the common-lisp-indent-function properties
+(setq lisp-indent-function 'common-lisp-indent-function)
+(cl-loop for symbol being the symbols
+         for cl-indent-rule = (get symbol 'common-lisp-indent-function)
+         for elisp-equivalent = (intern-soft (concat "cl-" (symbol-name symbol)))
+         when (and cl-indent-rule elisp-equivalent (fboundp elisp-equivalent))
+         do (put elisp-equivalent 'common-lisp-indent-function cl-indent-rule))
+
 
 ;; For EuLisp
 (put 'dynamic-let 'lisp-indent-function 1)
