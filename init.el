8bae220df89ff5a0d3f0a222bab840542fb0c319
T. Kurt Bond <tkurtbond@gmail.com>
2023-03-06 07:23:13 -0500

    Load tkb-microblog.el from its new home in the microblog repo.

--- init.el
+++ init.el
@@ -202,6 +202,7 @@
 (load-file "~/lib/emacs/tkb/tkb-time-expansion-keys.el")
 (load-file "~/lib/emacs/tkb/tkb-status-reports.el")
 (load-file "~/lib/emacs/tkb/tkb-blog.el")
+(load-file "~/Repos/microblog/emacs/tkb-microblog.el")
 
 
 ;;(setq epg-gpg-program "gpg2") ; on macOS with homebrew it's gpg.
