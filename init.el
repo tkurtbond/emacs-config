e4ad566fc9458395b0ceec8100f84129bc77902e
T. Kurt Bond <tkurtbond@gmail.com>
2023-10-12 17:37:33 -0400

    Start using desktop-save-mode.

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;;;; .emacs.el - Unified emacs startup.
 
+(desktop-save-mode 1)
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
