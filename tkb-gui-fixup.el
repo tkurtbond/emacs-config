406ec20ead3d99f63ebeabb44710e42933b74021
T. Kurt Bond <tkurtbond@gmail.com>
2023-11-24 11:13:16 -0500

    Separate tkb-gui-fixup into two functions, one interactive, and one not.

--- tkb-gui-fixup.el
+++ tkb-gui-fixup.el
@@ -4,25 +4,30 @@
 ;;; **NOT** PIXEL_SIZE: https://www.x.org/releases/X11R7.6/doc/xorg-docs/specs/XLFD/xlfd.html#pixel_size
 ;; ALSO: https://wiki.archlinux.org/title/X_Logical_Font_Description#Font_sizes
 
-(defun tkb-gui-fixup (prefix)
+(defun tkb-gui-prompt-fixup (prefix)
   "Fix up the emacs frames to have the right position and font."
   (interactive "P")
   (let ((point-size 10)
-        (height 55)
-        (width 80))
+        (width 80)
+        (height 55))
     (when prefix
       (setq point-size (read-number "Font Size?" point-size))
-      (setq height (read-number "Height?" height))
       (setq width (read-number "Width?" width))
-      (setq font (concat "-*-Go Mono-regular-normal-normal-*-*-"
-                         (number-to-string (* 10 point-size)) ; XLFD POINT_SIZE
-                            "-*-*-m-0-iso10646-1")))
-      
+      (setq height (read-number "Height?" height)))
+    (tkb-gui-fixup point-size width height)))
+
+
+(defun tkb-gui-fixup (point-size width height)
+  "Fix up the emacs frames to have the right position and font."
+  (let ((font (concat "-*-Go Mono-regular-normal-normal-*-*-"
+                      (number-to-string (* 10 point-size)) ; XLFD POINT_SIZE
+                      "-*-*-m-0-iso10646-1")))
     (cl-loop for f in (frame-list) do
          (set-frame-parameter f 'font font)
-         (set-frame-parameter f 'height height)
          (set-frame-parameter f 'width width)
+         (set-frame-parameter f 'height height)
          (setf (cdr (assq 'font default-frame-alist)) font)
-         (setf (cdr (assq 'height default-frame-alist)) height)
          (setf (cdr (assq 'width default-frame-alist)) width)
-         )))
+         (setf (cdr (assq 'height default-frame-alist)) height))))
+;; (tkb-gui-fixup 10 80 55)
+;; (tkb-gui-fixup 8 80 55)
