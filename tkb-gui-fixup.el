8f5723a452673851e6ecb4fd9762173facdde7a5
T. Kurt Bond <tkurtbond@gmail.com>
2023-11-28 22:16:12 -0500

    Make tkb-gui-fixup store its changes to the defaults.

--- tkb-gui-fixup.el
+++ tkb-gui-fixup.el
@@ -19,7 +19,7 @@
   "Fix up the emacs frames to have the right position and font."
   (interactive)
 
-  (unless font-name  (setq font-name  (tkb-select-mono-font tkb-default-font)))
+  (unless font-name  (setq font-name  (tkb-select-mono-font tkb-default-font-name)))
   (unless point-size (setq point-size (read-number "Font Size? " 10)))
   (unless width      (setq width      (read-number "Width? "
                                                    tkb-default-width)))
@@ -28,18 +28,27 @@
   (unless left       (setq left       (read-number "Left? " tkb-default-left)))
   (unless top        (setq top        (read-number "Top? " tkb-default-top)))
 
-  (let ((font (format "%s-%d" font-name point-size)))
-    (cl-loop for f in (frame-list) do
-         (set-frame-parameter f 'font   font)
-         (set-frame-parameter f 'width  width)
-         (set-frame-parameter f 'height height)
-         (set-frame-parameter f 'left   left)
-         (set-frame-parameter f 'top    top)
-         (setf (cdr (assq 'font   default-frame-alist)) font)
-         (setf (cdr (assq 'width  default-frame-alist)) width)
-         (setf (cdr (assq 'height default-frame-alist)) height)
-         (setf (cdr (assq 'left   default-frame-alist)) left)
-         (setf (cdr (assq 'top    default-frame-alist)) top))))
+  (setq tkb-default-font-name  font-name)
+  (setq tkb-default-point-size point-size)
+  (setq tkb-default-font       (tkb-mf point-size font-name))
+  (setq tkb-default-width      width)
+  (setq tkb-default-height     height)
+  (setq tkb-default-left       left)
+  (setq tkb-default-top        top)
+  (setq tkb-default-description "Set by tkb-gui-fixup")
+
+  (setf (cdr (assq 'font   default-frame-alist)) tkb-default-font)
+  (setf (cdr (assq 'width  default-frame-alist)) width)
+  (setf (cdr (assq 'height default-frame-alist)) height)
+  (setf (cdr (assq 'left   default-frame-alist)) left)
+  (setf (cdr (assq 'top    default-frame-alist)) top)
+
+  (cl-loop for f in (frame-list) do
+        (set-frame-parameter f 'font   tkb-default-font)
+        (set-frame-parameter f 'width  width)
+        (set-frame-parameter f 'height height)
+        (set-frame-parameter f 'left   left)
+        (set-frame-parameter f 'top    top)))
 
 (defun tkb-urlich ()
   (interactive)
@@ -50,42 +59,4 @@
 ;; (tkb-gui-fixup 8 80 55)
 
 
-;; See: https://emacs.stackexchange.com/a/50215
 
-(defun tkb-mono-fonts ()
-  (sort (seq-uniq (seq-filter (lambda (font)
-                                (when-let ((info (font-info font)))
-                                  (and (string-match-p "spacing=100" (aref info 1))
-                                       (not (or (string-match-p "Noto Color Emoji" (aref info 1))
-                                                (string-match-p "Noto Sans SignWriting" (aref info 1)))))))
-                              (font-family-list))
-                  #'string=)
-        #'string<))
-
-(defun tkb-list-mono-fonts ()
-  (interactive)
-  (let ((buf (get-buffer-create "*Mono Fonts*"))
-        (font-names (tkb-mono-fonts)))
-    (with-output-to-temp-buffer buf
-      (cl-loop for font-name in font-names
-         do (progn (princ font-name) (terpri))))))
-
-(defun tkb-display-mono-fonts ()
-  (interactive)
-  (loop for font-name in (tkb-mono-fonts)
-     do (progn
-          (set-frame-font font-name)
-          (unless (y-or-n-p (format "Font is %s.  Continue? " font-name))
-            (cl-return)))))
-
-(defun tkb-select-mono-font (&optional default)
-  (interactive)
-  (let ((fonts (tkb-mono-fonts)))
-    (completing-read "Monospace Font? " fonts)))
-
-(defun tkb-select-frame-font ()
-  (interactive)
-  (let ((font (tkb-select-mono-font)))
-    (if (not (string-blank-p font))
-        (set-frame-font font)
-      (message "No font selected!"))))
