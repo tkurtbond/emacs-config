69f38aef98478cd57e6ac86f08f2d6baa3443d7f
T. Kurt Bond <tkurtbond@gmail.com>
2024-05-03 19:20:48 -0400

    Add the name the start ssh-agent example used.

--- ssh-agent-for-emacs-under-sway.el
+++ ssh-agent-for-emacs-under-sway.el
@@ -1,1 +1,4 @@
+;; Example used:
+(setenv "SSH_AUTH_SOCK" (expand-file-name "~/.ssh/agent"))
+;; XDG/Fedora compatable?
 (setenv "SSH_AUTH_SOCK" (s-concat (getenv "XDG_RUNTIME_DIR") "/ssh-agent.socket"))
